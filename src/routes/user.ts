4f0c6ac33454123ceab74db9fb9494c326be07cf
mfauzanfikri <mfauzanfikri09@gmail.com>
2023-11-12 21:15:32 +0700

    update user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,11 +4,9 @@ import Controller from '../controllers/user';
 const router: Router = express.Router();
 
 router.get('/', Controller.get);
-
+router.get('/:id', Controller.getById);
 router.post('/', Controller.post);
-
 router.put('/', Controller.put);
-
 router.delete('/', Controller.delete);
 
 const userRouter = router;
